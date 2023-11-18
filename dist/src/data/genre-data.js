eeaefeb29f8a1c7874ec159287c3171ae22850bb
nguyễn công sơn <91744029+NguyenCongSon2402@users.noreply.github.com>
2023-11-18 21:20:01 +0700

    api

---
+++ dist/src/data/genre-data.js
@@ -0,0 +1,125 @@
+ "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const data = [
+    {
+        name: "Hành Động",
+        code: "action",
+    },
+    {
+        name: "Tình Cảm",
+        code: "romance",
+    },
+    {
+        name: "Hài Hước",
+        code: "comedy",
+    },
+    {
+        name: "Cổ Trang",
+        code: "historical",
+    },
+    {
+        name: "Tâm Lý",
+        code: "drama",
+    },
+    {
+        name: "Hình Sự",
+        code: "crime",
+    },
+    {
+        name: "Chiến Tranh",
+        code: "war",
+    },
+    {
+        name: "Thể Thao",
+        code: "sports",
+    },
+    {
+        name: "Võ Thuật",
+        code: "martial_arts",
+    },
+    {
+        name: "Viễn Tưởng",
+        code: "sci_fi",
+    },
+    {
+        name: "Phiêu Lưu",
+        code: "adventure",
+    },
+    {
+        name: "Khoa Học",
+        code: "science",
+    },
+    {
+        name: "Kinh Dị",
+        code: "horror",
+    },
+    {
+        name: "Âm Nhạc",
+        code: "music",
+    },
+    {
+        name: "Thần Thoại",
+        code: "mythology",
+    },
+    {
+        name: "Tài Liệu",
+        code: "documentary",
+    },
+    {
+        name: "Gia Đình",
+        code: "family",
+    },
+    {
+        name: "Chính kịch",
+        code: "drama",
+    },
+    {
+        name: "Bí ẩn",
+        code: "mystery",
+    },
+    {
+        name: "Học Đường",
+        code: "school",
+    },
+    {
+        name: "Kinh Điển",
+        code: "classic",
+    },
+    {
+        name: "Phim 18+",
+        code: "adult",
+    },
+    {
+        name:"Phim Bộ",
+        code:"@series",
+    },
+    {
+        name:"Phim Lẻ",
+        code:"@single",
+    },
+    {
+        name:"Phim Hoạt Hình",
+        code:"@hoathinh",
+    },
+    {
+        name:"TV Show",
+        code:"@tvshows",
+    },
+    {
+        name:"Phim VietSub",
+        code:"@series",
+    },
+    {
+        name:"Phim Thuyết Minh",
+        code:"@series",
+    },
+    {
+        name:"Phim Bộ Đang Chiếu",
+        code:"@series",
+    },
+    {
+        name:"Phim Bộ Đã Hoàn Thành",
+        code:"@series",
+    }
+];
+exports.default = data;
